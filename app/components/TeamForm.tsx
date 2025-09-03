c0232a31d92fe47fdde7f1045147fd6814a7e234
Joshua Gretton <jb.gretton@googlemail.com>
2025-09-03 11:13:01 +0100

    Basic TeamForm
    Implemented basic form for updating team name. Added icons for better
    UI.

---
+++ app/components/TeamForm.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useMatchStore } from "@/store";
+import { LoaderCircleIcon } from "lucide-react";
+import { useState } from "react";
+
+interface TeamFormProps {
+	teamType: "home" | "away";
+}
+
+export default function TeamForm({ teamType }: TeamFormProps) {
+	const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+	const { setTeamName, match } = useMatchStore();
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setIsSubmitting(true);
+
+		try {
+			const formData = new FormData(event.currentTarget);
+			const teamName = formData.get("teamName") as string;
+			// add delay to simulate server delay.
+			await new Promise((resolve) => setTimeout(resolve, 1000)); // this line needs to be after formdata.get() otherwise it throws errors.
+			setTeamName(teamType, teamName);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
+	const { homeTeam, awayTeam } = match;
+	return (
+		<form className="grid gap-3 w-full" onSubmit={handleSubmit}>
+			<span className="font-semibold text-lg">
+				{teamType === "home" ? "Home Team" : "Away Team"}
+			</span>
+			<label className="grid gap-1" htmlFor="teamName">
+				<span className="text-sm font-medium">Team Name</span>
+				<input
+					type="text"
+					defaultValue={
+						teamType === "home" ? homeTeam.teamName : awayTeam.teamName
+					}
+					className="rounded-md p-2 border border-gray-200 disabled:opacity-45 disabled:cursor-not-allowed w-full"
+					name="teamName"
+					id="teamName"
+					disabled={isSubmitting}
+				/>
+			</label>
+
+			<button
+				className="rounded-md bg-violet-600 text-white py-2 px-2 font-medium hover:bg-violet-700 cursor-pointer focus:outline-2 focus:outline-violet-700 focus: outline-offset-2 disabled:bg-violet-400 disabled:cursor-not-allowed grid place-items-center"
+				disabled={isSubmitting}
+			>
+				{isSubmitting ? (
+					<span className="flex items-center gap-2">
+						<LoaderCircleIcon className="size-3 animate-spin" /> Submitting...
+					</span>
+				) : (
+					"Submit"
+				)}
+			</button>
+		</form>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import TeamForm from "./components/TeamForm";
+
 export default function Home() {
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<span>VolleyRotation Tracker</span>
+
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-lg">
+				<TeamForm teamType="home" />
+				<TeamForm teamType="away" />
+			</div>
 		</div>
 	);
 }
